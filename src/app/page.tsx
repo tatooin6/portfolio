2b4ee2734f04a6f6b9c1a2f58d096b776f1f106d
Antonio Pantoja <tato_fos@hotmail.com>
2025-01-07 17:53:18 -0400

    Add white text to Title when its dark theme activated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-start p-24">
       <div className="z-10 w-full max-w-5xl flex flex-col items-center justify-center font-mono text-sm">
-        <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Tato</h2>
-        <p className="text-xl lg:static flex w-full justify-start bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:bg-zinc-800/30 dark:from-inherit lg:rounded-xl lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <h2 className="text-2xl/7 font-bold dark:text-gray-200 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          Tato
+        </h2>
+        <p className="text-xl lg:static flex w-full justify-start bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:from-inherit dark:text-gray-200 lg:rounded-xl lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Full Stack Web Developer & Educator
         </p> 
-        <p className="pb-4">Experienced professional with a passion for efficient 
+        <p className="pb-4 dark:text-gray-200">Experienced professional with a passion for efficient 
           solutions and sharing technical expertise.</p>
       </div>
     </main>
