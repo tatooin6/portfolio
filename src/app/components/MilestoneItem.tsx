f4206720ecf29d014df76d330687438d401c4717
Antonio Pantoja <tato_fos@hotmail.com>
2025-01-26 20:56:51 -0400

    Add improvements
    - Included expand and show less button
    - Improved visibility of json data
    - Updated colors to make it visible on light mode

--- src/app/components/MilestoneItem.tsx
+++ src/app/components/MilestoneItem.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { Milestone } from "./Timeline";
-import { MdExpandMore, MdExpandLess } from "react-icons/md";
+import { MdExpandMore, MdExpandLess, MdArrowForward } from "react-icons/md";
 import { titleFormat } from "@/utils/dateUtils";
 
 const MilestoneItem = ({ milestone }: { milestone: Milestone }) => {
@@ -13,11 +13,24 @@ const MilestoneItem = ({ milestone }: { milestone: Milestone }) => {
       <li className="mb-10 ms-4">
         <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
         <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-          {titleFormat(milestone.startDate)}
+          <div className="flex flex-row">
+            <span>{titleFormat(milestone.startDate)}</span>
+            <div className="px-4">
+              <MdArrowForward />
+            </div>
+            <span>{titleFormat(milestone.endDate)}</span>
+          </div>
         </time>
-        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-          {milestone.title}
-        </h3>
+        <div className="flex flex-row">
+          <h3 className="text-lg font-semibold dark:text-white pr-4">
+            {milestone.title}
+          </h3>
+          {milestone.company && (
+            <div className="flex flex-col items-center justify-center text-gray-400 dark:text-gray-500">
+              <h3 className="text-sm font-thin">{milestone.company}</h3>
+            </div>
+          )}
+        </div>
         <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
           {milestone.summary}
         </p>
@@ -30,7 +43,7 @@ const MilestoneItem = ({ milestone }: { milestone: Milestone }) => {
             )}
             <button
               onClick={() => setIsExpanded((isExp) => !isExp)}
-              className="inline-flex items-center py-2 text-sm font-medium text-gray-900 hover:text-blue-700 focus:z-10 focus:outline-none focus:text-blue-700 dark:text-gray-400 dark:hover:text-white"
+              className="inline-flex items-center py-2 text-sm font-medium text-gray-400 hover:text-blue-700 focus:z-10 focus:outline-none focus:text-blue-700 dark:text-gray-400 dark:hover:text-white"
             >
               {isExpanded ? (
                 <>
--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -1,27 +1,27 @@
-import { titleFormat } from "@/utils/dateUtils"
-import MilestoneItem from "./MilestoneItem"
+import MilestoneItem from "./MilestoneItem";
 
 export interface Milestone {
-    startDate: Date
-    endDate: Date
-    title: string
-    company?: string
-    summary: string
-    extendedDescription?: string
-};
-
-const Timeline = ({ milestones }: { milestones: Milestone[] }) => {
-    return (
-        <div>
-            <ol className="relative border-s border-gray-200 dark:border-gray-700">
-                {
-                    milestones.map(milestone => (
-                        <MilestoneItem key={milestone.startDate.toString()} milestone={milestone} />
-                    ))
-                }
-            </ol>
-        </div>
-    )
+  startDate: Date;
+  endDate: Date;
+  title: string;
+  company?: string;
+  summary: string;
+  extendedDescription?: string;
 }
 
-export default Timeline
+const Timeline = ({ milestones }: { milestones: Milestone[] }) => {
+  return (
+    <div>
+      <ol className="relative border-s border-gray-200 dark:border-gray-700">
+        {milestones.map((milestone) => (
+          <MilestoneItem
+            key={milestone.startDate.toString()}
+            milestone={milestone}
+          />
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default Timeline;
