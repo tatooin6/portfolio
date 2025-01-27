377b01fde3b22ca5e4c581c6dd05c83f5ea9ec8e
Antonio Pantoja <tato_fos@hotmail.com>
2025-01-26 21:22:29 -0400

    Fix dark mode text on timeline text

--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -12,8 +12,8 @@ export interface Milestone {
 const Timeline = ({ milestones }: { milestones: Milestone[] }) => {
   return (
     <div>
-      <h1 className="text-lg">Experience Timeline</h1>
-      <p className="pb-6">Latest Experience</p>
+      <h1 className="text-lg dark:text-gray-200">Experience Timeline</h1>
+      <p className="pb-6 dark:text-gray-200">Latest Experience</p>
       <ol className="relative border-s border-gray-700 dark:border-white">
         {milestones.map((milestone) => (
           <MilestoneItem
@@ -22,7 +22,7 @@ const Timeline = ({ milestones }: { milestones: Milestone[] }) => {
           />
         ))}
       </ol>
-      <p>Earliest Experience</p>
+      <p className="dark:text-gray-200">Earliest Experience</p>
     </div>
   );
 };
