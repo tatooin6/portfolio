f5d5eaca54036628d29428fb412a5f8e31d658eb
Antonio Pantoja <tato_fos@hotmail.com>
2025-01-28 16:30:22 -0400

    Improve earliest experience indicator position on Timeline

--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -12,13 +12,14 @@ export interface Milestone {
 const Timeline = ({ milestones }: { milestones: Milestone[] }) => {
   return (
     <div>
-      <h1 className="text-lg dark:text-gray-200">Experience Timeline</h1>
-      <p className="pb-6 dark:text-gray-200">Latest Experience</p>
+      <h1 className="text-lg pb-5 dark:text-gray-200">Experience Timeline</h1>
+      <p className="pb-1 dark:text-gray-200">Latest Experience</p>
       <ol className="relative border-s border-gray-700 dark:border-white">
-        {milestones.map((milestone) => (
+        {milestones.map((milestone, index) => (
           <MilestoneItem
             key={milestone.startDate.toString()}
             milestone={milestone}
+            isLast={index === milestones.length - 1}
           />
         ))}
       </ol>
